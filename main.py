eca5c0d87f5302a7c5453c1ecf44b244ad269012
Primoria <137751691+Primoria@users.noreply.github.com>
2023-09-15 21:38:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import os
-
-from art import *
+from colorama import Fore, Back, Style
 from prozac import Prozac
 
 def run():
     startup()
+    prints()
     Prozac().listen()
 
 def startup():
-    os.system('color')
+    os.system("color")
     os.system(f"title Prozac")
-    print(text2art("Prozac", font="doom"))
+
+def prints():
+    print(f"""{Fore.CYAN}
+             \t\t\t\t    ▄▄▄·▄▄▄        ·▄▄▄▄• ▄▄▄·  ▄▄· 
+             \t\t\t\t   ▐█ ▄█▀▄ █·▪     ▪▀·.█▌▐█ ▀█ ▐█ ▌▪
+             \t\t\t\t    ██▀·▐▀▀▄  ▄█▀▄ ▄█▀▀▀•▄█▀▀█ ██ ▄▄
+             \t\t\t\t   ▐█▪·•▐█•█▌▐█▌.▐▌█▌▪▄█▀▐█ ▪▐▌▐███
+             \t\t\t\t   .▀   .▀  ▀ ▀█▄▀▪·▀▀▀ • ▀  ▀ ·▀▀
+          {Style.RESET_ALL}""")
+    
+    print(
+    f"{Fore.LIGHTBLACK_EX}[" + f"{Fore.CYAN}x" + f"{Fore.LIGHTBLACK_EX}]{Style.RESET_ALL}"
+    " Join my discord server: " +
+    f"{Fore.WHITE}discord.gg/bsNKqvxvE2{Style.RESET_ALL}"
+    )
 
 run()
